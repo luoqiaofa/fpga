ddeb568e31de95de30c13ca37787c0e748779d07
luoqiaofa <luoqiaofa@163.com>
2020-07-05 14:01:08 +0800

    rename bit define and bit ctl state machine define

--- i2c/i2c-reg-def.v
+++ i2c/i2c-reg-def.v
@@ -1,32 +1,32 @@
 // I2CADR
-localparam BIT_ADR_ADDR_HI = 7;
-localparam BIT_ADR_ADDR_LO = 1;
-localparam BIT_ADR_ADDR_RW = 0;
+localparam BIT_ADDR_HI = 7;
+localparam BIT_ADDR_LO = 1;
+localparam BIT_ADDR_RW = 0;
 
 // I2CFDR
-localparam BIT_FDR_FDR_NUM_HI  = 6;
-localparam BIT_FDR_FDR_NUM_LO  = 0;
+localparam BIT_FDR_HI  = 6;
+localparam BIT_FDR_LO  = 0;
 
 // I2CCR
-localparam BIT_CR_MEN  = 7;
-localparam BIT_CR_MIEN = 6;
-localparam BIT_CR_MSTA = 5;
-localparam BIT_CR_MTX  = 4;
-localparam BIT_CR_TXAK = 4;
-localparam BIT_CR_RSTA = 2;
-localparam BIT_CR_BCST = 0;
+localparam BIT_MEN  = 7;
+localparam BIT_MIEN = 6;
+localparam BIT_MSTA = 5;
+localparam BIT_MTX  = 4;
+localparam BIT_TXAK = 4;
+localparam BIT_RSTA = 2;
+localparam BIT_BCST = 0;
 
 // I2CSR
-localparam BIT_SR_MCF   = 7;
-localparam BIT_SR_MAAS  = 6;
-localparam BIT_SR_MBB   = 5;
-localparam BIT_SR_MAL   = 4;
-localparam BIT_SR_BCSTM = 3;
-localparam BIT_SR_SRW   = 2;
-localparam BIT_SR_MIF   = 1;
-localparam BIT_SR_RXAK  = 0;
+localparam BIT_MCF   = 7;
+localparam BIT_MAAS  = 6;
+localparam BIT_MBB   = 5;
+localparam BIT_MAL   = 4;
+localparam BIT_BCSTM = 3;
+localparam BIT_SRW   = 2;
+localparam BIT_MIF   = 1;
+localparam BIT_RXAK  = 0;
 // I2CDR
 // I2CDFSR
-localparam BIT_DFSRR_DFSR_HI = 6;
-localparam BIT_DFSRR_DFSR_LO = 0;
+localparam BIT_DFSR_HI = 6;
+localparam BIT_DFSR_LO = 0;
 
