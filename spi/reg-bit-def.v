44fce26d23fdfe3215d00939d170795d61b81101
qiaofa.luo <qiaofa.luo@szhz-it.com>
2020-12-21 18:18:19 +0800

    reg and module tmp mody

--- spi/reg-bit-def.v
+++ spi/reg-bit-def.v
@@ -1,3 +1,17 @@
+// ESPI regist addr
+localparam ADDR_SPMODE       = 8'h00;
+localparam ADDR_SPIE         = 8'h04;
+localparam ADDR_SPIM         = 8'h08;
+localparam ADDR_SPCOM        = 8'h0c;
+localparam ADDR_SPITF        = 8'h10;
+localparam ADDR_SPIRF        = 8'h14;
+localparam ADDR_REV          = 8'h18;
+localparam ADDR_REV          = 8'h1c;
+localparam ADDR_SPMODE0      = 8'h20;
+localparam ADDR_SPMODE1      = 8'h24;
+localparam ADDR_SPMODE2      = 8'h28;
+localparam ADDR_SPMODE3      = 8'h2c;
+
 // 1. SPMODE
 localparam SPMODE_EN         = 31;
 localparam SPMODE_LOOP       = 30;
@@ -9,59 +23,59 @@ localparam SPMODE_RXTHR_HI   = 4;
 localparam SPMODE_RXTHR_LO   = 0;
 
 // 2. SPIE
-localparam SPIE_RXCNT_HI   = 29;
-localparam SPIE_RXCNT_LO   = 24;
-localparam SPIE_TXCNT_HI   = 21;
-localparam SPIE_TXCNT_LO   = 16;
-localparam SPIE_TXE        = 15;
-localparam SPIE_DON        = 14;
-localparam SPIE_RXT        = 13;
-localparam SPIE_RXF        = 12;
-localparam SPIE_TXT        = 11;
-localparam SPIE_RNE        = 9;
-localparam SPIE_TNF        = 8;
+localparam SPIE_RXCNT_HI     = 29;
+localparam SPIE_RXCNT_LO     = 24;
+localparam SPIE_TXCNT_HI     = 21;
+localparam SPIE_TXCNT_LO     = 16;
+localparam SPIE_TXE          = 15;
+localparam SPIE_DON          = 14;
+localparam SPIE_RXT          = 13;
+localparam SPIE_RXF          = 12;
+localparam SPIE_TXT          = 11;
+localparam SPIE_RNE          = 9;
+localparam SPIE_TNF          = 8;
 
 // 3. SPIM
-localparam SPIM_TXE        = 15;
-localparam SPIM_DON        = 14;
-localparam SPIM_RXT        = 13;
-localparam SPIM_RXF        = 12;
-localparam SPIM_TXT        = 11;
-localparam SPIM_22         = 10;
-localparam SPIM_RNE        = 9;
-localparam SPIM_TNF        = 8;
+localparam SPIM_TXE          = 15;
+localparam SPIM_DON          = 14;
+localparam SPIM_RXT          = 13;
+localparam SPIM_RXF          = 12;
+localparam SPIM_TXT          = 11;
+localparam SPIM_22           = 10;
+localparam SPIM_RNE          = 9;
+localparam SPIM_TNF          = 8;
 
 // 4. SPCOM
-localparam SPCOM_CS_HI      = 31;
-localparam SPCOM_CS_LO      = 30;
-localparam SPCOM_RxDELAY    = 29;
-localparam SPCOM_DO         = 28;
-localparam SPCOM_HLD        = 26;
-localparam SPCOM_TO         = 27;
-localparam SPCOM_RxSKIP_HI  = 23;
-localparam SPCOM_RxSKIP_LO  = 16;
-localparam SPCOM_TRANLEN_HI = 15;
-localparam SPCOM_TRANLEN_LO = 0;
+localparam SPCOM_CS_HI       = 31;
+localparam SPCOM_CS_LO       = 30;
+localparam SPCOM_RxDELAY     = 29;
+localparam SPCOM_DO          = 28;
+localparam SPCOM_HLD         = 26;
+localparam SPCOM_TO          = 27;
+localparam SPCOM_RxSKIP_HI   = 23;
+localparam SPCOM_RxSKIP_LO   = 16;
+localparam SPCOM_TRANLEN_HI  = 15;
+localparam SPCOM_TRANLEN_LO  = 0;
 
 // 5. SPITF
 
 // 6. SPIRF
 
 // 7. ESPI_SPMODE0;
-localparam SPMODEx_CI         = 31;
-localparam SPMODEx_CP         = 30;
-localparam SPMODEx_REV        = 29;
-localparam SPMODEx_DIV16      = 28;
-localparam SPMODEx_PM_HI      = 27;
-localparam SPMODEx_PM_LO      = 24;
-localparam SPMODEx_ODD        = 23;
-localparam SPMODEx_POL        = 20;
-localparam SPMODEx_LEN_HI     = 19;
-localparam SPMODEx_LEN_LO     = 16;
-localparam SPMODEx_CSBEF_HI   = 15;
-localparam SPMODEx_CSBEF_LO   = 12;
-localparam SPMODEx_CSAFT_HI   = 11;
-localparam SPMODEx_CSAFT_LO   = 8;
-localparam SPMODEx_CSCG_HI    = 7;
-localparam SPMODEx_CSCG_LO    = 3;
+localparam SPMODEx_CI        = 31;
+localparam SPMODEx_CP        = 30;
+localparam SPMODEx_REV       = 29;
+localparam SPMODEx_DIV16     = 28;
+localparam SPMODEx_PM_HI     = 27;
+localparam SPMODEx_PM_LO     = 24;
+localparam SPMODEx_ODD       = 23;
+localparam SPMODEx_POL       = 20;
+localparam SPMODEx_LEN_HI    = 19;
+localparam SPMODEx_LEN_LO    = 16;
+localparam SPMODEx_CSBEF_HI  = 15;
+localparam SPMODEx_CSBEF_LO  = 12;
+localparam SPMODEx_CSAFT_HI  = 11;
+localparam SPMODEx_CSAFT_LO  = 8;
+localparam SPMODEx_CSCG_HI   = 7;
+localparam SPMODEx_CSCG_LO   = 3;
 
--- spi/spi_module.v
+++ spi/spi_module.v
@@ -1,185 +1,22 @@
-`include "timescale.v"
-/*
- * this spi module will compatible to Fresscale powerpc ESPI
- * ESPI normal operation, exclude RapidS
- * ESPI_SPMODE
- * ESPI_SPIE
- * ESPI_SPIM
- * ESPI_SPCOM
- * ESPI_SPITF
- * ESPI_SPIRF
- * ESPI_SPMODE0
- * ESPI_SPMODE1
- * ESPI_SPMODE2
- * ESPI_SPMODE3
- */
-/*
- * ***************************************************************************
- * I_SPI_MODE
- * 31   RST
- * 30:8 reserved
- * 7
- * 6
- * 5
- * 4
- * 3 tx_start one risine puls to triger start
- * 2 : 1  chip seledt 0 1 2 3 for 4 cs
- * 0  EN
- * ***************************************************************************
- */
-module spi_module # (
-    parameter N = 32,
-    parameter NCS = 4
-)
+module spi_one_char
+#(CHAR_LEN_MAX = 16)
 (
-    input                I_CLK,
-    input                I_RST_N,
-    input                I_MISO,
-    output               O_MOSI,
-    output               O_SCK,
-    output     [NCS-1:0] O_CS,
-    input      [N-1:0]   I_TX_DATA,
-    output reg [N-1:0]   O_RX_DATA,
-    input      [N-1:0]   I_SPI_MODE,
-    input      [N-1:0]   I_SPI_SCK_DIV,
-    output reg [N-1:0]   O_SPI_STATUS
-
-    input      [N-1:0]   I_SPMODE,
-    output reg [N-1:0]   O_SPIE,
-    input      [N-1:0]   I_SPIM,
-    input      [N-1:0]   I_SPCOM,
-    input      [N-1:0]   I_SPITF,
-    input      [N-1:0]   I_SPIRF,
-    input      [N-1:0]   I_SPMODE0,
-    input      [N-1:0]   I_SPMODE1,
-    input      [N-1:0]   I_SPMODE2,
-    input      [N-1:0]   I_SPMODE3
+    input                          sysclk,
+    input                          reset_n,
+    input                          enable,
+    input                          CPOL,
+    input                          CPHA,
+    input                          LOOP,
+    input                          CHAR_EN, // characters in bits length
+    input  wire                    SCK,
+    output wire                    MISO,
+    input  wire                    MOSI,
+    input                          sck_pos_edge,
+    input                          sck_neg_edge,
+    input                          go,
+    output                         done,
+    input       [CHAR_LEN_MAX-1:0] data_out,
+    output reg  [CHAR_LEN_MAX-1:0] data_in,
 );
 `include "reg-bit-def.v"
-
-// O_SPI_STATUS
-localparam CHAR_TRANS_DONE   = 0;
-
-localparam CS_HI       = NCS - 1;
-
-//value of the ceiling of the log base 2.
-function integer clogb2 (input integer x);
-    integer i;
-    begin
-        clogb2 = 1;
-        for (i = 0; 2**i < x; i = i + 1)
-        begin
-            clogb2 = i + 1;
-        end
-    end
-endfunction
-
-localparam N_BITS_CNT = clogb2(N);
-localparam NCS_CNT    = clogb2(NCS);
-// localparam N_BITS_CNT = 3;
-// localparam NCS_CNT   = 2;
-
-reg r_sck;
-reg [N - 1:0] sck_hlf_cnt;
-reg [N - 1:0] r_hlf_div;
-reg [N - 1:0] r_miso_fifo;
-// reg [N - 1:0] r_status;
-reg [N_BITS_CNT - 1 : 0] r_bit_cnt;
-reg r_en;
-reg r_mosi;
-reg r_miso;
-reg [NCS - 1:0] r_cs;
-
-assign O_MOSI = r_mosi;
-assign O_SCK = r_sck;
-assign O_CS[NCS - 1:0] = r_cs[NCS - 1:0];
-// assign O_SPI_STATUS = r_status;
-
-
-always @(posedge I_SPI_MODE[BIT_TRANS_START])
-begin
-    if (I_SPI_MODE[BIT_TRANS_START])
-        O_SPI_STATUS[CHAR_TRANS_DONE] <= 1'b0;
-end
-
-always @(negedge r_en or negedge r_sck)
-begin
-    if (!r_en) begin
-        sck_hlf_cnt <= {N{1'b0}};
-        r_bit_cnt <= N - 1;
-        r_mosi <= I_TX_DATA[N - 1];
-    end
-    else
-    begin 
-        r_bit_cnt <= r_bit_cnt - 1;
-        case (r_bit_cnt)
-            0: 
-            begin 
-            O_SPI_STATUS[CHAR_TRANS_DONE] <= 1'b1;
-            O_RX_DATA <= r_miso_fifo;
-            end
-            default : O_SPI_STATUS[CHAR_TRANS_DONE] <= 1'b0;
-
-        endcase
-        r_miso_fifo[r_bit_cnt] <= r_miso;
-    end
-end
-
-always @(negedge r_en or posedge r_sck)
-begin
-    if (!r_en) begin
-        r_miso_fifo[r_bit_cnt] <= 1'b1;
-    end
-    else
-        r_miso_fifo[r_bit_cnt] <= r_miso;
-end
-
-always @(posedge I_CLK or negedge I_RST_N)
-begin
-    if (!I_RST_N) begin
-        O_RX_DATA <= {N{1'b1}};
-        O_SPI_STATUS <= {N{1'b0}};
-        r_miso_fifo <= {N{1'b1}};
-        r_miso <= I_MISO;
-        r_mosi <= I_TX_DATA[N - 1];
-        r_en <= 0;
-        r_sck <= 0;
-        r_cs <= {NCS{1'b1}};
-        sck_hlf_cnt <= {N{1'b0}};
-        r_hlf_div <= {N{1'b0}};
-        r_bit_cnt <= N - 1;
-        r_mosi <= I_TX_DATA[N - 1];
-    end
-    else
-    begin
-        r_hlf_div  <= (I_SPI_SCK_DIV >> 1);
-        r_en <= I_SPI_MODE[BIT_EN];
-
-        if (sck_hlf_cnt == (r_hlf_div - 1)) begin
-            sck_hlf_cnt <= {N{1'b0}};
-            if (!O_SPI_STATUS[CHAR_TRANS_DONE]) 
-                if (I_SPI_MODE[BIT_EN])
-                    r_sck <= ~r_sck;
-                else
-                    r_sck <= 1;
-        end
-        else begin
-            sck_hlf_cnt <= sck_hlf_cnt + 1;
-        end
-
-        r_mosi <= I_TX_DATA[r_bit_cnt];
-        r_miso <= I_MISO;
-        
-        if (I_SPI_MODE[BIT_EN])
-            case (I_SPI_MODE[BIT_CS_HI:BIT_CS_LO])
-                2'h0 : r_cs[NCS - 1:0] <= {{NCS - 1{1'b1}}, 1'b0};
-                2'h1 : r_cs[NCS - 1:0] <= {{NCS - 2{1'b1}}, 1'b0, {1{1'b1}}};
-                2'h2 : r_cs[NCS - 1:0] <= {{NCS - 3{1'b1}}, 1'b0, {2{1'b1}}};
-                2'h3 : r_cs[NCS - 1:0] <= {{NCS - 4{1'b1}}, 1'b0, {3{1'b1}}};
-            endcase
-        else
-            r_cs[NCS - 1:0] <= {NCS{1'b1}};
-    end
-end
-endmodule
 
