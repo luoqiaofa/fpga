74c13737308bf48ac9304ef46a288bb47e21cd2a
qiaofa.luo <qiaofa.luo@szhz-it.com>
2020-12-22 16:07:35 +0800

    module spi_trx_one_char tx ok

--- spi/spi_module.v
+++ spi/spi_module.v
@@ -6,32 +6,287 @@ module spi_trx_one_char
     input  wire        S_ENABLE,  // enable
     input  wire        S_CPOL,    // clock polary
     input  wire        S_CPHA,    // clock phase, the first edge or second
+    input  wire        S_TX_ONLY, // transmit only
     input  wire        S_LOOP,    // internal loopback mode
     input  wire        S_REV,     // msb first or lsb first
     input  wire [3:0]  S_CHAR_LEN,// characters in bits length
-    input  wire        S_SPI_SCK,
-    output wire        S_SPI_MISO,
-    input  wire        S_SPI_MOSI,
+    input  wire [7:0]  S_NDIVIDER,// clock divider
+    output wire        S_SPI_SCK,
+    input  wire        S_SPI_MISO,
+    output wire        S_SPI_MOSI,
     input  wire        S_CHAR_GO,
     output wire        S_CHAR_DONE,
     input  wire [15:0] S_WCHAR,   // output character
     output wire [15:0] S_RCHAR    // input character
 );
 `include "reg-bit-def.v"
+localparam CHAR_NBITS = CHAR_LEN_MAX;
 
-reg sck_pos_edge;
-reg sck_neg_edge;
+reg go;        // start transmit
+reg done;
+reg last_clk;  // last clock 
+reg dout;
+reg [CHAR_NBITS - 1: 0] data_in;
+reg [4:0] bit_cnt;
+reg [4: 0] shift_cnt;
+reg [CHAR_NBITS:0] shift_tx;
+reg [CHAR_NBITS:0] shift_rx;
+wire [1:0] spi_mode;
+wire pos_edge; // positive edge flag
+wire neg_edge; // negtive edge flag
+wire pos_edge_rx;         // positive edge flag
+wire neg_edge_rx;         // positive edge flag
 
-spi_clk_gen # (.N(8)) clk_gen (
-    .sysclk(sysclk),       // system clock input
-    .rst_n(rst_n),         // module reset
-    .enable(enable),       // module enable
+assign S_SPI_MOSI = dout;
+assign S_RCHAR    = data_in;
+assign S_CHAR_DONE = done;
+assign spi_mode = {S_CPHA, S_CPOL};
+assign pos_edge_rx = S_SPI_SCK;
+assign neg_edge_rx = S_SPI_SCK;
+
+spi_clk_gen # (.C_DIVIDER_WIDTH(8)) clk_gen_char (
+    .sysclk(S_SYSCLK),       // system clock input
+    .rst_n(S_RESETN),         // module reset
+    .enable(S_ENABLE),       // module enable
     .go(go),               // start transmit
-    .CPOL(CPOL),           // clock polarity
+    .CPOL(S_CPOL),           // clock polarity
     .last_clk(last_clk),   // last clock 
-    .divider_i(divider_i), // divider;
-    .clk_out(clk_out),     // clock output
+    .divider_i(S_NDIVIDER), // divider;
+    .clk_out(S_SPI_SCK),     // clock output
     .pos_edge(pos_edge),   // positive edge flag
     .neg_edge(neg_edge)    // negtive edge flag
 );
+
+always @(posedge S_SYSCLK or negedge S_RESETN)
+begin
+    if (!S_RESETN)
+    begin
+        go   <= 0;
+        done <= 0;
+        last_clk <= 0;
+        dout <= 1'b0;
+        data_in  <= {CHAR_NBITS{1'b1}};
+        bit_cnt   <= CHAR_NBITS - 1;
+        shift_cnt <= CHAR_NBITS - 1;
+        shift_rx <= {1'b1, {CHAR_NBITS{1'b1}}};
+        shift_tx <= {1'b0, {CHAR_NBITS{S_WCHAR}}};
+    end
+    else
+    begin
+        done <= 0;
+        if (S_ENABLE)
+        begin
+            // last_clk <= last_clk;
+            go <= S_CHAR_GO;
+            dout <= shift_tx[shift_cnt];
+            shift_rx <= shift_rx;
+            shift_cnt <= shift_cnt;
+            if (!go)
+            begin
+                case (spi_mode)
+                    2'b00:
+                    begin
+                        shift_cnt <= CHAR_NBITS - 1;
+                    end
+                    2'b01:
+                    begin
+                        shift_cnt <= CHAR_NBITS - 1;
+                    end
+                    2'b10:
+                    begin
+                        shift_cnt <= CHAR_NBITS;
+                    end
+                    2'b11:
+                    begin
+                        shift_cnt <= CHAR_NBITS;
+                    end
+                endcase // case (spi_mode)
+            end // end of (0 == S_ENABLE)
+        end // end of if (S_ENABLE)
+    end
+end
+
+always @(negedge neg_edge)
+begin
+    case (spi_mode)
+        2'b00:
+        begin
+            if (last_clk & go) 
+            begin
+                go <= 0;
+                last_clk <= 0;
+            end
+        end
+        2'b01:
+        begin
+            bit_cnt <= bit_cnt - 5'h1;
+            if (bit_cnt == 5'h0) 
+            begin
+                last_clk <= 1;
+                bit_cnt <= (CHAR_NBITS - 1);
+            end
+        end
+        2'b10:
+        begin
+            if (last_clk & go) 
+            begin
+                go <= 0;
+                last_clk <= 0;
+            end
+        end
+        2'b11:
+        begin
+            bit_cnt <= bit_cnt - 5'h1;
+            if (bit_cnt == 5'h0) 
+            begin
+                last_clk <= 1;
+                bit_cnt <= (CHAR_NBITS - 1);
+            end
+        end
+    endcase
+end
+
+always @(negedge pos_edge)
+begin
+    case (spi_mode)
+        2'b00:
+        begin
+            bit_cnt <= bit_cnt - 5'h1;
+            if (bit_cnt == 5'h0) 
+            begin
+                last_clk <= 1;
+                bit_cnt <= (CHAR_NBITS - 1);
+            end
+        end
+        2'b01:
+        begin
+            if (last_clk & go) 
+            begin
+                go <= 0;
+                last_clk <= 0;
+            end
+        end
+        2'b10:
+        begin
+            bit_cnt <= bit_cnt - 5'h1;
+            if (bit_cnt == 5'h0) 
+            begin
+                last_clk <= 1;
+                bit_cnt <= (CHAR_NBITS - 1);
+            end
+        end
+        2'b11:
+        begin
+            if (last_clk & go) 
+            begin
+                go <= 0;
+                last_clk <= 0;
+            end
+        end
+    endcase
+end
+
+always @(posedge pos_edge_rx)
+begin
+    if (S_ENABLE)
+    begin
+        case (spi_mode)
+            2'b00:
+            begin
+                if (S_LOOP)
+                begin
+                    shift_rx[shift_cnt] <= dout;
+                end
+                if (0 == shift_cnt)
+                begin
+                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+                end
+            end
+            2'b01:
+            begin
+                shift_cnt <= shift_cnt - 1;
+                if (0 == shift_cnt)
+                begin
+                    done <= 1;
+                    shift_cnt <= CHAR_NBITS - 1;
+                end
+            end
+            2'b10:
+            begin
+                shift_cnt <= shift_cnt - 1;
+                if (0 == shift_cnt)
+                begin
+                    shift_cnt <= CHAR_NBITS;
+                end
+            end
+            2'b11:
+            begin
+                if (S_LOOP)
+                begin
+                    shift_rx[shift_cnt] <= dout;
+                end
+                if (0 == shift_cnt)
+                begin
+                    done <= 1;
+                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+                end
+            end
+        endcase
+    end
+end
+
+always @(negedge neg_edge_rx)
+begin
+    if (S_ENABLE)
+    begin
+        case (spi_mode)
+            2'b00:
+            begin
+                shift_cnt <= shift_cnt - 1;
+                if (0 == shift_cnt)
+                begin
+                    done <= 1;
+                    shift_cnt <= CHAR_NBITS - 1;
+                end
+            end
+            2'b01:
+            begin
+                if (S_LOOP)
+                begin
+                    shift_rx[shift_cnt] <= dout;
+                end
+                if (0 == shift_cnt)
+                begin
+                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+                end
+            end
+            2'b10:
+            begin
+                if (S_LOOP)
+                begin
+                    shift_rx[shift_cnt] <= dout;
+                end
+                if (0 == shift_cnt)
+                begin
+                    done <= 1;
+                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+                end
+            end
+            2'b11:
+            begin
+                shift_cnt <= shift_cnt - 1;
+                if (0 == shift_cnt)
+                begin
+                    shift_cnt <= CHAR_NBITS;
+                end
+                if (S_LOOP)
+                begin
+                    shift_rx[shift_cnt] <= dout;
+                end
+            end
+        endcase
+    end
+end
+
+endmodule
 
--- spi/spi_tb.v
+++ spi/spi_tb.v
@@ -28,100 +28,101 @@ module spi_tb;
     wire pos_edge_rx;         // positive edge flag
     wire neg_edge_rx;         // positive edge flag
 
-assign mosi = dout;
+pullup pullup_miso (miso);
+// assign mosi = dout;
 assign spi_mode = {CPHA, CPOL};
 assign pos_edge_rx = clk_out;
 assign neg_edge_rx = clk_out;
 
-always @(negedge neg_edge or negedge rst_n)
-begin
-    if (!rst_n || !enable)
-        ;
-    else 
-    begin
-        case (spi_mode)
-            2'b00:
-            begin
-                if (last_clk & go) 
-                begin
-                    go <= 0;
-                    last_clk <= 0;
-                end
-            end
-            2'b01:
-            begin
-                bit_cnt <= bit_cnt - 5'h1;
-                if (bit_cnt == 5'h0) 
-                begin
-                    last_clk <= 1;
-                    bit_cnt <= (CHAR_NBITS - 1);
-                end
-            end
-            2'b10:
-            begin
-                if (last_clk & go) 
-                begin
-                    go <= 0;
-                    last_clk <= 0;
-                end
-            end
-            2'b11:
-            begin
-                bit_cnt <= bit_cnt - 5'h1;
-                if (bit_cnt == 5'h0) 
-                begin
-                    last_clk <= 1;
-                    bit_cnt <= (CHAR_NBITS - 1);
-                end
-            end
-        endcase
-    end
-end
-
-always @(negedge pos_edge or negedge rst_n)
-begin
-    if (!rst_n || !enable)
-        bit_cnt <= (CHAR_NBITS - 1);
-    else 
-    begin
-        case (spi_mode)
-            2'b00:
-            begin
-                bit_cnt <= bit_cnt - 5'h1;
-                if (bit_cnt == 5'h0) 
-                begin
-                    last_clk <= 1;
-                    bit_cnt <= (CHAR_NBITS - 1);
-                end
-            end
-            2'b01:
-            begin
-                if (last_clk & go) 
-                begin
-                    go <= 0;
-                    last_clk <= 0;
-                end
-            end
-            2'b10:
-            begin
-                bit_cnt <= bit_cnt - 5'h1;
-                if (bit_cnt == 5'h0) 
-                begin
-                    last_clk <= 1;
-                    bit_cnt <= (CHAR_NBITS - 1);
-                end
-            end
-            2'b11:
-            begin
-                if (last_clk & go) 
-                begin
-                    go <= 0;
-                    last_clk <= 0;
-                end
-            end
-        endcase
-    end
-end
+// always @(negedge neg_edge or negedge rst_n)
+// begin
+//     if (!rst_n || !enable)
+//         ;
+//     else 
+//     begin
+//         case (spi_mode)
+//             2'b00:
+//             begin
+//                 if (last_clk & go) 
+//                 begin
+//                     go <= 0;
+//                     last_clk <= 0;
+//                 end
+//             end
+//             2'b01:
+//             begin
+//                 bit_cnt <= bit_cnt - 5'h1;
+//                 if (bit_cnt == 5'h0) 
+//                 begin
+//                     last_clk <= 1;
+//                     bit_cnt <= (CHAR_NBITS - 1);
+//                 end
+//             end
+//             2'b10:
+//             begin
+//                 if (last_clk & go) 
+//                 begin
+//                     go <= 0;
+//                     last_clk <= 0;
+//                 end
+//             end
+//             2'b11:
+//             begin
+//                 bit_cnt <= bit_cnt - 5'h1;
+//                 if (bit_cnt == 5'h0) 
+//                 begin
+//                     last_clk <= 1;
+//                     bit_cnt <= (CHAR_NBITS - 1);
+//                 end
+//             end
+//         endcase
+//     end
+// end
+// 
+// always @(negedge pos_edge or negedge rst_n)
+// begin
+//     if (!rst_n || !enable)
+//         bit_cnt <= (CHAR_NBITS - 1);
+//     else 
+//     begin
+//         case (spi_mode)
+//             2'b00:
+//             begin
+//                 bit_cnt <= bit_cnt - 5'h1;
+//                 if (bit_cnt == 5'h0) 
+//                 begin
+//                     last_clk <= 1;
+//                     bit_cnt <= (CHAR_NBITS - 1);
+//                 end
+//             end
+//             2'b01:
+//             begin
+//                 if (last_clk & go) 
+//                 begin
+//                     go <= 0;
+//                     last_clk <= 0;
+//                 end
+//             end
+//             2'b10:
+//             begin
+//                 bit_cnt <= bit_cnt - 5'h1;
+//                 if (bit_cnt == 5'h0) 
+//                 begin
+//                     last_clk <= 1;
+//                     bit_cnt <= (CHAR_NBITS - 1);
+//                 end
+//             end
+//             2'b11:
+//             begin
+//                 if (last_clk & go) 
+//                 begin
+//                     go <= 0;
+//                     last_clk <= 0;
+//                 end
+//             end
+//         endcase
+//     end
+// end
 
 initial
 begin            
@@ -129,172 +130,200 @@ begin
     $dumpvars(0, spi_tb);    //tb模块名称
 end
 
-spi_clk_gen # (.C_DIVIDER_WIDTH(8)) clk_gen (
-    .sysclk(sysclk),       // system clock input
-    .rst_n(rst_n),         // module reset
-    .enable(enable),       // module enable
-    .go(go),               // start transmit
-    .CPOL(CPOL),           // clock polarity
-    .last_clk(last_clk),   // last clock 
-    .divider_i(divider_i), // divider;
-    .clk_out(clk_out),     // clock output
-    .pos_edge(pos_edge),   // positive edge flag
-    .neg_edge(neg_edge)    // negtive edge flag
+//spi_clk_gen # (.C_DIVIDER_WIDTH(8)) clk_gen (
+//    .sysclk(sysclk),       // system clock input
+//    .rst_n(rst_n),         // module reset
+//    .enable(enable),       // module enable
+//    .go(go),               // start transmit
+//    .CPOL(CPOL),           // clock polarity
+//    .last_clk(last_clk),   // last clock 
+//    .divider_i(divider_i), // divider;
+//    .clk_out(clk_out),     // clock output
+//    .pos_edge(pos_edge),   // positive edge flag
+//    .neg_edge(neg_edge)    // negtive edge flag
+//);
+wire s_done;
+wire  [15:0] rdata;
+spi_trx_one_char #(.CHAR_LEN_MAX(16))
+inst_spi_trx_ch
+(
+    .S_SYSCLK(sysclk),  // platform clock
+    .S_RESETN(rst_n),  // reset
+    .S_ENABLE(enable),  // enable
+    .S_CPOL(CPOL),    // clock polary
+    .S_CPHA(CPHA),    // clock phase, the first edge or second
+    .S_TX_ONLY(1'b0), // transmit only
+    .S_LOOP(LOOP),    // internal loopback mode
+    .S_REV(1'b0),     // msb first or lsb first
+    .S_CHAR_LEN(4'h7),// characters in bits length
+    .S_NDIVIDER(divider_i),// clock divider
+    .S_SPI_SCK(clk_out),
+    .S_SPI_MISO(miso),
+    .S_SPI_MOSI(mosi),
+    .S_CHAR_GO(go),
+    .S_CHAR_DONE(s_done),
+    .S_WCHAR(16'h55aa),   // output character
+    .S_RCHAR(rdata)    // input character
 );
+
+always @(posedge s_done)
+begin
+    go <= 0;
+end
 
 // 100 MHz axi clock input
 always @(sysclk)
     #5 sysclk <= !sysclk;
 
-always @(posedge pos_edge_rx or negedge rst_n)
-begin
-    if (!rst_n || !enable)
-    begin
-        data_in  <= {CHAR_NBITS{1'b1}};
-    end
-    else
-    begin
-        if (enable)
-        begin
-        case (spi_mode)
-            2'b00:
-            begin
-                if (LOOP)
-                begin
-                    shift_rx[shift_cnt] <= dout;
-                end
-                if (0 == shift_cnt)
-                begin
-                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
-                end
-            end
-            2'b01:
-            begin
-                shift_cnt <= shift_cnt - 1;
-                if (0 == shift_cnt)
-                begin
-                    shift_cnt <= CHAR_NBITS - 1;
-                end
-            end
-            2'b10:
-            begin
-                shift_cnt <= shift_cnt - 1;
-                if (0 == shift_cnt)
-                begin
-                    shift_cnt <= CHAR_NBITS;
-                end
-            end
-            2'b11:
-            begin
-                if (LOOP)
-                begin
-                    shift_rx[shift_cnt] <= dout;
-                end
-                if (0 == shift_cnt)
-                begin
-                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
-                end
-            end
-        endcase
-        end
-    end
-end
-
-always @(negedge neg_edge_rx or negedge rst_n)
-begin
-    if (!rst_n || !enable)
-    begin
-    end
-    else
-    begin
-        if (enable)
-        begin
-        case (spi_mode)
-            2'b00:
-            begin
-                shift_cnt <= shift_cnt - 1;
-                if (0 == shift_cnt)
-                begin
-                    shift_cnt <= CHAR_NBITS - 1;
-                end
-            end
-            2'b01:
-            begin
-                if (LOOP)
-                begin
-                    shift_rx[shift_cnt] <= dout;
-                end
-                if (0 == shift_cnt)
-                begin
-                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
-                end
-            end
-            2'b10:
-            begin
-                if (LOOP)
-                begin
-                    shift_rx[shift_cnt] <= dout;
-                end
-                if (0 == shift_cnt)
-                begin
-                    data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
-                end
-            end
-            2'b11:
-            begin
-                shift_cnt <= shift_cnt - 1;
-                if (0 == shift_cnt)
-                begin
-                    shift_cnt <= CHAR_NBITS;
-                end
-                if (LOOP)
-                begin
-                    shift_rx[shift_cnt] <= dout;
-                end
-            end
-        endcase
-        end
-    end
-end
-
-always @(posedge sysclk or negedge rst_n)
-begin
-    if (!rst_n || !enable)
-    begin
-        dout <= 1'b0;
-        shift_rx <= {1'b1, {CHAR_NBITS{1'b1}}};
-        shift_tx <= {1'b0, data_out};
-        case (spi_mode)
-            2'b00:
-            begin
-                shift_cnt <= CHAR_NBITS - 1;
-            end
-            2'b01:
-            begin
-                shift_cnt <= CHAR_NBITS - 1;
-            end
-            2'b10:
-            begin
-                shift_cnt <= CHAR_NBITS;
-            end
-            2'b11:
-            begin
-                shift_cnt <= CHAR_NBITS;
-            end
-        endcase
-    end
-    else
-    begin
-        if (enable)
-        begin
-            // last_clk <= last_clk;
-            // go <= go;
-            dout <= shift_tx[shift_cnt];
-            shift_rx <= shift_rx;
-            shift_cnt <= shift_cnt;
-        end
-    end
-end
+// always @(posedge pos_edge_rx or negedge rst_n)
+// begin
+//     if (!rst_n || !enable)
+//     begin
+//         data_in  <= {CHAR_NBITS{1'b1}};
+//     end
+//     else
+//     begin
+//         if (enable)
+//         begin
+//         case (spi_mode)
+//             2'b00:
+//             begin
+//                 if (LOOP)
+//                 begin
+//                     shift_rx[shift_cnt] <= dout;
+//                 end
+//                 if (0 == shift_cnt)
+//                 begin
+//                     data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+//                 end
+//             end
+//             2'b01:
+//             begin
+//                 shift_cnt <= shift_cnt - 1;
+//                 if (0 == shift_cnt)
+//                 begin
+//                     shift_cnt <= CHAR_NBITS - 1;
+//                 end
+//             end
+//             2'b10:
+//             begin
+//                 shift_cnt <= shift_cnt - 1;
+//                 if (0 == shift_cnt)
+//                 begin
+//                     shift_cnt <= CHAR_NBITS;
+//                 end
+//             end
+//             2'b11:
+//             begin
+//                 if (LOOP)
+//                 begin
+//                     shift_rx[shift_cnt] <= dout;
+//                 end
+//                 if (0 == shift_cnt)
+//                 begin
+//                     data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+//                 end
+//             end
+//         endcase
+//         end
+//     end
+// end
+// 
+// always @(negedge neg_edge_rx or negedge rst_n)
+// begin
+//     if (!rst_n || !enable)
+//     begin
+//     end
+//     else
+//     begin
+//         if (enable)
+//         begin
+//         case (spi_mode)
+//             2'b00:
+//             begin
+//                 shift_cnt <= shift_cnt - 1;
+//                 if (0 == shift_cnt)
+//                 begin
+//                     shift_cnt <= CHAR_NBITS - 1;
+//                 end
+//             end
+//             2'b01:
+//             begin
+//                 if (LOOP)
+//                 begin
+//                     shift_rx[shift_cnt] <= dout;
+//                 end
+//                 if (0 == shift_cnt)
+//                 begin
+//                     data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+//                 end
+//             end
+//             2'b10:
+//             begin
+//                 if (LOOP)
+//                 begin
+//                     shift_rx[shift_cnt] <= dout;
+//                 end
+//                 if (0 == shift_cnt)
+//                 begin
+//                     data_in  <= {shift_rx[CHAR_NBITS-1:1], dout};
+//                 end
+//             end
+//             2'b11:
+//             begin
+//                 shift_cnt <= shift_cnt - 1;
+//                 if (0 == shift_cnt)
+//                 begin
+//                     shift_cnt <= CHAR_NBITS;
+//                 end
+//                 if (LOOP)
+//                 begin
+//                     shift_rx[shift_cnt] <= dout;
+//                 end
+//             end
+//         endcase
+//         end
+//     end
+// end
+// 
+// always @(posedge sysclk or negedge rst_n)
+// begin
+//     if (!rst_n || !enable)
+//     begin
+//         dout <= 1'b0;
+//         shift_rx <= {1'b1, {CHAR_NBITS{1'b1}}};
+//         shift_tx <= {1'b0, data_out};
+//         case (spi_mode)
+//             2'b00:
+//             begin
+//                 shift_cnt <= CHAR_NBITS - 1;
+//             end
+//             2'b01:
+//             begin
+//                 shift_cnt <= CHAR_NBITS - 1;
+//             end
+//             2'b10:
+//             begin
+//                 shift_cnt <= CHAR_NBITS;
+//             end
+//             2'b11:
+//             begin
+//                 shift_cnt <= CHAR_NBITS;
+//             end
+//         endcase
+//     end
+//     else
+//     begin
+//         if (enable)
+//         begin
+//             // last_clk <= last_clk;
+//             // go <= go;
+//             dout <= shift_tx[shift_cnt];
+//             shift_rx <= shift_rx;
+//             shift_cnt <= shift_cnt;
+//         end
+//     end
+// end
 
 initial
 begin            
@@ -306,7 +335,7 @@ begin
     enable     <= 0;      // module enable
     go         <= 0;      // start transmit
     CPOL       <= 0;      // clock polarity
-    CPHA       <= 0;      // clock phase
+    CPHA       <= 1;      // clock phase
     LOOP       <= 1;
     last_clk   <= 0;      // last clock 
     divider_i  <= 0;      // divider;
@@ -346,6 +375,7 @@ begin
     #1000
     $stop;
 end
+
 
 endmodule
 
